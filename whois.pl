d7e2507f9c8c16b4753ec0a29f5f9c122afbc5a8
John Moses <moses.john.r@gmail.com>
2011-06-23 09:03:24 -0500

    added help menu

--- whois.pl
+++ whois.pl
@@ -1,4 +1,19 @@
 use Net::Whois::Raw;
+use Cwd;
+use strict;
+binmode STDOUT, ":utf8";
 
-my $domainInfo= whois($ARGV[0]);
-print $domainInfo;
+if($ARGV[0] =~ /-h/){
+    my $dir = getdcwd();
+    print "perl-command-line-whois"
+         ."\nview whois information for given domain"
+         ."\nUsage: $dir\\whois.pl [option] [domain]"
+         ."\n\t-h\t\tthis help menu"
+         ."\n\t-o\t\tquery whois information for domain.  e.g. whois.pl -o ycombinator.org";
+    exit;
+} 
+
+if($ARGV[0] =~ /-o/){
+    my $domainInfo= whois($ARGV[1]);
+    print $domainInfo;
+}
